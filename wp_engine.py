c3d3eaad1a9d0f7751dfad6cfa71d31e5afea117
manoflogan <manoflogan@gmail.com>
2019-01-26 15:21:28 -0800

    First pass of unit tests.

--- wp_engine.py
+++ wp_engine.py
@@ -1,12 +1,14 @@
-"""Entry point to wp engine coding test.
-
+"""
+Entry point to wp engine coding test.
 
 The implementation combines results from api and the csv file to write a single
 combined output to a file whose path is provided by the user.
 
 To call this function:
-   python wp_engine.py --input '<fully qualified_file_path_to_input_csv_file>' --output '<fully qualified path to output csv>'
+   python wp_engine.py --input '<fully qualified_file_path_to_input_csv_file>' --output '<fully qualified path to output csv>'  # noqa: E501
 """
+
+
 import argparse
 import csv
 import datetime
@@ -22,14 +24,13 @@ def fetch_account_status(account_id):
     """Makes the http request ot fetch account status for account id.
 
     Args:
-        account_id: integer; account id for which the accounts information is to
-            be fetched
+        account_id: integer; account id for which the accounts information is
+            to be fetched
 
     Returns:
         dictionary representing representing the parsed output
     """
     res_object = requests.get(_ACCOUNTS_URL.format(account_id=account_id))
-
     return res_object.json() if res_object.status_code == 200 else {}
 
 
@@ -37,8 +38,8 @@ def read_csv_file(input_csv_file_path):
     """Reads a csv file to return a tuple of csv rows.
 
     Args:
-        input_csv_file_path: fully qualified path of the input csv file to be
-            read.
+        input_csv_file_path: fully qualified path of the
+            input csv file to be read
 
     Yields:
         tuple representing the account id, first name, and created date in that
@@ -47,19 +48,22 @@ def read_csv_file(input_csv_file_path):
     with open(input_csv_file_path, 'r', encoding='utf-8') as file_path:
         csv_reader = csv.reader(file_path)
         for row in itertools.islice(csv_reader, 1, None):
-            yield (int(row[0]), row[2],
-                   datetime.datetime.strftime(datetime.datetime.strptime(row[-1], '%m/%d/%y'), '%Y-%m-%d'))
+            yield (
+                int(row[0]), row[2],
+                datetime.datetime.strftime(
+                    datetime.datetime.strptime(row[-1], '%m/%d/%y'),
+                    '%Y-%m-%d'))
 
 
 def collate_similar_data(input_csv_file_path, output_file_path):
     """Collates input data from multiple resources to write to putput file.
 
-    input_csv_file_path: fully qualified file path of the input
-        file to read
-    output_csv_file_path: fully qualified path of the output file
-        path to be written to
+    Args:
+        input_csv_file_path: fully qualified file path of the input
+            file to read
+        output_csv_file_path: fully qualified path of the output file
+            path to be written to
     """
-    rows = []
     with open(output_file_path, 'w') as file_object:
         csv_writer = csv.writer(file_object, delimiter=',')
         csv_writer.writerow(
@@ -74,7 +78,7 @@ def collate_similar_data(input_csv_file_path, output_file_path):
                 datetime.datetime.strftime(
                     datetime.datetime.strptime(
                         account_status.get('created_on'), '%Y-%m-%d'),
-                    '%Y-%m-%d')))
+                    '%Y-%m-%d') if account_status.get('created_on') else ''))
 
 
 if __name__ == '__main__':
---
+++ wp_engine_test.py
@@ -0,0 +1,101 @@
+import datetime
+import os
+from unittest import mock
+
+import pytest
+
+import wp_engine
+
+
+@pytest.fixture(scope='module')
+def input_csv_file():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.normpath(os.path.join(current_path, 'input.csv'))
+
+
+@pytest.fixture(scope='module')
+def output_csv_file():
+    current_path = os.path.dirname(os.path.realpath(__file__))
+    return os.path.normpath(os.path.join(current_path, 'output.csv'))
+
+
+def test_read_csv_file(input_csv_file):
+    expected = [
+        (12345, 'Lex', '2011-01-12'),
+        (8172, 'Victor', '2014-11-19'),
+        (1924, 'Max', '2012-02-29'),
+        (222222, 'Ra\'s', '2012-03-01'),
+        (48213, 'Wilson', '2015-07-07'),
+        (918299, 'Norman', '2014-04-29'),
+        (88888, 'Otto', '2013-08-08')]
+    actual = [row for row in wp_engine.read_csv_file(input_csv_file)]
+    assert actual == expected
+
+
+def test_fetch_account_status__success():
+    response_object = mock.Mock()
+    response_object.status_code = 200
+    expected = {
+        'account_id': 12345,
+        'status': 'fraud',
+        'created_on': '2015-03-20'}
+    response_object.json = mock.MagicMock(return_value=expected)
+    with mock.patch('requests.get',
+                    return_value=response_object) as mock_request:
+        response = wp_engine.fetch_account_status(12345)
+        assert (
+            mock_request.call_args[0][0] == (
+                'http://interview.wpengine.io/v1/accounts/12345'))
+        assert mock_request.call_count == 1
+        assert response == expected
+
+
+def test_fetch_account_status__failure():
+    response_object = mock.Mock()
+    response_object.status_code = 500
+    with mock.patch('requests.get',
+                    return_value=response_object) as mock_request:
+        response = wp_engine.fetch_account_status(12345)
+        assert (
+            mock_request.call_args[0][0] == (
+                'http://interview.wpengine.io/v1/accounts/12345'))
+        assert mock_request.call_count == 1
+        assert response == {}
+
+
+def read_csv_file(input_file_path):
+    rows = [(12345, 'lexcorp', 'Lex', datetime.date(2011, 1, 12))]
+    for row in rows:
+        yield row
+
+
+def test_collate_similar_data(input_csv_file, output_csv_file):
+    try:
+        rows = []
+        csv_writer_object = mock.Mock()
+        csv_writer_object.writerow = lambda row: rows.append(row)
+        with mock.patch('csv.writer',
+                        return_value=csv_writer_object) as mock_writer, \
+                mock.patch(
+                    'wp_engine.read_csv_file',
+                    side_effect=read_csv_file) as mock_read_csv_file, \
+                mock.patch(
+                    'wp_engine.fetch_account_status', return_value={
+                        'account_id': 12345,
+                        'status': 'fraud',
+                        'created_on': '2015-03-20'
+                    }) as mock_fetch_account_status:
+            wp_engine.collate_similar_data(input_csv_file, output_csv_file)
+            assert mock_writer.call_count == 1
+            assert mock_writer.call_args[0][0] is not None
+            assert mock_read_csv_file.call_count == 1
+            assert mock_read_csv_file.call_args[0][0] == input_csv_file
+            assert mock_fetch_account_status.call_count == 1
+            assert mock_fetch_account_status.call_args[0][0] == 12345
+            assert rows == [
+                ('Account ID', 'First Name', 'Created On', 'Status',
+                 'Status Set On'),
+                (12345, 'lexcorp', 'Lex', datetime.date(2011, 1, 12), 'fraud',
+                 '2015-03-20')]
+    finally:
+        os.remove(output_csv_file)
